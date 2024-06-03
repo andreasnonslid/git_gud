b36514f2c715f412e2d63c11a01b543e6d5991b2
Andreas Nonslid Håvardsen <48278625+andreasnonslid@users.noreply.github.com>
2024-06-03 21:54:08 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,1 +1,8 @@
-return require('git_patch')
+local git_patch = require('git_patch')
+
+return {
+    setup = git_patch.setup,
+    stage_and_commit = git_patch.stage_and_commit,
+    stage_hunk = git_patch.stage_hunk,
+    git_add_patch = git_patch.git_add_patch
+}
